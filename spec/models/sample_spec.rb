5bd1a75c1ba62453aace1f2d31fcac965468f51e
na9amura <4410504+na9amura@users.noreply.github.com>
2020-06-04 15:53:33 +0900

    add tests

--- spec/models/sample_spec.rb
+++ spec/models/sample_spec.rb
@@ -1,10 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'Sample' do
-  describe 'pass' do
-    it { expect(1 + 1).to eq 2 }
+RSpec.describe Sample, type: :model do
+  describe 'samples' do
+    describe 'pass' do
+      it { expect(1 + 1).to eq 2 }
+    end
+    describe 'fails' do
+      it { expect(2 + 2).to eq 5 }
+    end
   end
-  describe 'fails' do
-    it { expect(2 + 2).to eq 5 }
+
+  describe 'records' do
+    let!(:record) { Sample.create(name: :my_name) }
+
+    it { expect(Sample.all).to contain_exactly(record) }
   end
 end
